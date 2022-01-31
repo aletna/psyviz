8283b2ca1dc9590dd662e895b4508aea7b01eda8
aletna <dennis.antela@gmail.com>
2022-01-31 02:25:29 -0800

    add logs

--- src/components/graphs/CalendarChart.js
+++ src/components/graphs/CalendarChart.js
@@ -6,14 +6,12 @@ import { ResponsiveTimeRange } from "@nivo/calendar";
 // website examples showcase many properties,
 // you'll often use just a few of them.
 export default function CalendarChart({ data /* see data tab */ }) {
-  console.log("final callllll", data);
   const calMin = data.reduce(function (prev, curr) {
     return prev.day < curr.day ? prev : curr;
   });
   const calMax = data.reduce(function (prev, curr) {
     return prev.day > curr.day ? prev : curr;
   });
-  console.log(">>>>", calMin, calMax);
   return (
     <ResponsiveTimeRange
       data={data}
